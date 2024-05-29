f260c6b88ea47e10e00bb80f9171e47c2241fc83
Yash Vishnoi47 <vikasvishnoi005@gmail.com>
2024-05-29 05:25:53 -0700

    Some Final Edits

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navbar = document.querySelector(".navbar");
+  
+    menuToggle.addEventListener("click", function() {
+      navbar.classList.toggle("open");
+    });
+  });
+  
